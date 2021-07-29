9075900c4e2588dccc4b4e4db2e44b33394af4b5
filippo <fsiri2000@gmail.com>
2021-07-29 19:02:15 +0200

    inizio

---
+++ graph.cpp
@@ -0,0 +1,193 @@
+#include "graph.h"
+
+using namespace graph;
+
+struct graph::halfEdgeVertex{
+    vertexNode* vertexPtr;
+    Weight weight;
+    halfEdgeVertex* next;
+};
+
+struct graph::vertexNode{
+    Label label;
+    halfEdgeVertex* adjList;
+    vertexNode* next;
+    bool visited;
+};
+
+/*******************************************************************************************************/
+// Aux
+/*******************************************************************************************************/
+Graph getNode(const Label l, const Graph& g){
+    for(Graph tmp = g; !isEmpty(tmp); tmp = tmp -> next){
+        if(tmp -> label == l)
+            return tmp;
+    }
+    return emptyGraph;
+}
+
+// Ritorna un cammino tra un nodo ed un altro
+// Il cammino da "v1" a "v2" alla fine sara' in "path"
+// e la lunghezza sara' in "len".
+// Si assume che il chiamante fornisca inizialmente un cammino vuoto.
+//
+// La funzione rappresenta una variante della visita DFS
+bool findPathAux(Label v2, listNS::List &path, int&len, const Graph& g){
+    if(isEmpty(g))
+        return false;
+    g -> visited = true;
+    if(g -> label == v2)
+        return true;
+    adjVertex tmp = g -> adjList;
+    while(tmp != emptyAdjVertex){
+        if(!(tmp -> vertexPtr -> visited) && findPathAux(v2, path, len, tmp -> vertexPtr)){
+            len += tmp -> weight;
+            listNS::addFront("", tmp -> vertexPtr -> label, path);
+
+            return true;
+        }
+        tmp = tmp -> next;
+    }
+    return false;
+}
+
+adjVertex createAdjVertex(Weight w, adjVertex adj, const Graph& to){
+    adjVertex tmp = new halfEdgeVertex;
+    tmp -> next = adj;
+    tmp -> vertexPtr = to;
+    tmp -> weight = w;
+    return tmp;
+}
+
+
+void setFalse(const Graph& g){
+    Graph tmp = g;
+    while(!isEmpty(tmp)){
+        tmp -> visited = false;
+        tmp = tmp -> next;
+    }
+}
+
+/*******************************************************************************************************/
+// Graph
+/*******************************************************************************************************/
+
+// Restituisce il grafo vuoto
+Graph graph::createEmptyGraph()
+{
+    return emptyGraph;
+}
+
+// Aggiunge nuovo vertice con etichetta la stringa. Fallisce se gia' presente
+bool graph::addVertex(Label l, Graph& g) {
+    if(!isEmpty(getNode(l, g)))
+        return false;
+    Graph tmp = new vertexNode;
+    tmp -> label = l;
+    tmp -> adjList = emptyAdjVertex;
+    tmp -> visited = false;
+    tmp -> next = g;
+    g = tmp;
+    return true;
+}
+
+// Aggiunge un arco di peso "w" tra i nodi con etichetta "f" e "t". Fallisce se esiste gia' l'arco
+// se i nodi non esistono nel grafo e se si tenta di inserire un arco tra un nodo ed esso stesso
+bool graph::addEdge(Label from, Label to, Weight w, const Graph& g) {
+    Graph nodeFrom = getNode(from, g);
+    Graph nodeTo = getNode(to, g);
+    if(isEmpty(nodeFrom) || isEmpty(nodeTo))
+        return false;
+    nodeFrom -> adjList = createAdjVertex(w, nodeFrom -> adjList, nodeTo);
+    return true;
+}
+
+// Restituisce true se il grafo e' vuoto, false altrimenti
+bool graph::isEmpty(const Graph& g)
+{
+    return g == emptyGraph;
+}
+
+// Ritorna il numero di vertici del grafo
+int graph::numVertices(const Graph& g){
+    Graph tmp = g;
+    int count=0;
+    while(!isEmpty(tmp)){
+        tmp = tmp -> next;
+        count++;
+    }
+    return count;
+}
+
+// Ritorna il numero di archi del grafo
+int graph::numEdges(const Graph& g){
+    int count = 0;
+    for(Graph tmp = g; !isEmpty(tmp); tmp = tmp -> next)
+        count += nodeDegree(tmp);
+    return count;
+}
+
+// Calcola e ritorna il grado del nodo. Se fallisce (il nodo non esiste)
+// restituisce -1
+int graph::nodeDegree(Label l, const Graph& g) {
+    Graph node = getNode(l, g);
+    if(isEmpty(node))
+        return -1;
+    int count = 0;
+    for(adjVertex tmp = node -> adjList; tmp != emptyAdjVertex; tmp = tmp -> next)
+        count++;
+    return count;
+}
+
+// Verifica se esiste un arco da v1 a v2
+bool graph::areAdjacent(Label v1, Label v2, const Graph& g) {
+    Graph tmpGraph = getNode(v1, g);
+    if(isEmpty(tmpGraph))
+        return false;
+    for(adjVertex tmp = tmpGraph -> adjList; tmp != emptyAdjVertex; tmp = tmp -> next)
+        if(tmp -> vertexPtr -> label == v2)
+            return true;
+    return false;
+}
+
+// Restituisce la lista di adiacenza di un vertice
+std::list<Label> graph::adjacentList(Label v1, const Graph& g) {
+    std::list<Label> l = {};
+    Graph tmpGraph = getNode(v1, g);
+    if(isEmpty(tmpGraph))
+        return lst;
+    for(adjVertex tmp = tmpGraph -> adjList; tmp != emptyAdjVertex; tmp = tmp -> next)
+        l.push_front(tmpAdj -> vertexPtr -> label);
+    return l;
+}
+
+bool graph::changeLabel(Label oldLabel, Label newLabel, const Graph &g) {
+    Graph tmp = getNode(oldLabel, g);
+    if(isEmpty(tmp))
+        return false;
+    tmp -> label = newLabel;
+    return true;
+}
+
+std::list<std::pair<Label, Label> > graph::allEdge(const Graph& g){
+    std::list<std::pair<Label, Label> > l = {};
+    for(Graph tmp = g; !isEmpty(tmp); tmp = tmp -> next){
+        for(adjVertex  adj = tmp -> adjList; adj != emptyAdjVertex; adj = adj -> next)
+            l.push_back(std::make_pair(tmpGraph -> label, tmpAdj -> vertexPtr -> label));
+    return l;
+}
+
+/*******************************************************************************************************/
+// Stampa il grafo
+void printGraph(const graph::Graph& g) {
+    Graph tmpGraph = g;
+    while(!isEmpty(tmpGraph)){
+        adjVertex tmpAdj = tmpGraph -> adjList;
+        cout<<tmpGraph -> label<<":"<<endl;
+        while(tmpAdj != emptyAdjVertex){
+            cout<<"\t"<<tmpAdj -> vertexPtr -> label<<" "<<tmpAdj -> weight<<endl;
+            tmpAdj = tmpAdj -> next;
+        }
+        tmpGraph = tmpGraph -> next;
+    }
+}
---
+++ graph.h
@@ -0,0 +1,68 @@
+#include <cstddef>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <list>
+#include <utility>
+
+using namespace std;
+
+namespace graph {
+
+    typedef string Label;
+    typedef int Weight;
+
+    struct vertexNode; // da definire nel file graph.cpp
+    struct halfEdgeVertex;
+
+    typedef vertexNode *Graph; // un grafo e' identificato dal puntatore al primo vertice inserito
+    typedef halfEdgeVertex *adjVertex;
+
+    const Graph emptyGraph = NULL;
+    const adjVertex emptyAdjVertex = NULL;
+
+// createEmptyGraph restituisce il grafo vuoto
+    Graph createEmptyGraph();
+
+// Aggiunge nuovo vertice con etichetta la stringa. Fallisce se gia' presente
+    bool addVertex(Label, Graph &);
+
+// Aggiunge nuovo arco tra i due nodi con etichette le due stringe e peso
+// l'intero. Fallisce se non sono presenti tutti e due i nodi o se l'arco
+// tra i due e' gia' presente.
+    bool addEdge(Label, Label, Weight, const Graph &);
+
+// Restituisce true se il grafo e' vuoto, false altrimenti
+    bool isEmpty(const Graph &);
+
+// Ritorna il numero di vertici del grafo
+    int numVertices(const Graph &);
+
+// Ritorna il numero di archi del grafo
+    int numEdges(const Graph &);
+
+// Calcola e ritorna il grado del nodo. Se fallisce (il nodo non esiste)
+// restituisce -1
+    int nodeDegree(Label, const Graph &);
+
+// Verifica se due vertici sono adiacenti (ovvero se esiste un arco)
+    bool areAdjacent(Label, Label, const Graph &);
+
+// Ritorna la lista di adiacenza di un vertice
+    std::list<Label> adjacentList(Label, const Graph &);
+
+// Calcola, se esiste, un cammino tra due vertici
+    void findPath(Label, Label, listNS::List &, int &, const Graph &g);
+
+//Se esiste, cambia il label del nodo
+    bool changeLabel(Label, Label, const Graph &g);
+
+    bool removeVertex(Label,  Graph &g);
+
+    listNS::List allEdge(const Graph &);
+}
+
+/* Funzioni che non caratterizzano il TDD Graph, ma che servono per input/output */
+//tree::Tree readFromFile(string);
+void printGraph(const graph::Graph&);
